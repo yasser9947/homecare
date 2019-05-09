6002ce80adb310583dc0e20959b267026ac307df
Yasir S <yasser9946.1@gmail.com>
2019-05-10 02:01:03 +0300

    2

--- care/src/FOQ.js
+++ care/src/FOQ.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react'
+import Spinner from 'react-bootstrap/Spinner'
 
 export default class FOQ extends Component {
   render() {
     return (
       <div>
-        <h1>FOQ</h1>
-        <h1>FOQ</h1>
-        <h1>FOQ</h1>
-        <h1>FOQ</h1>
-        <h1>FOQ</h1>
-        <h1>FOQ</h1>
-        <h1>FOQ</h1>
-        <h1>FOQ</h1>
-        <h1>FOQ</h1>
-        <h1>FOQ</h1>
-        <h1>FOQ</h1>
-        <h1>FOQ</h1>
+ <>
+  <Spinner animation="border" variant="primary" />
+  <Spinner animation="border" variant="secondary" />
+  <Spinner animation="border" variant="success" />
+  <Spinner animation="border" variant="danger" />
+  <Spinner animation="border" variant="warning" />
+  <Spinner animation="border" variant="info" />
+  <Spinner animation="border" variant="light" />
+  <Spinner animation="border" variant="dark" />
+  <Spinner animation="grow" variant="primary" />
+  <Spinner animation="grow" variant="secondary" />
+  <Spinner animation="grow" variant="success" />
+  <Spinner animation="grow" variant="danger" />
+  <Spinner animation="grow" variant="warning" />
+  <Spinner animation="grow" variant="info" />
+  <Spinner animation="grow" variant="light" />
+  <Spinner animation="grow" variant="dark" />
+</>;
 
       </div>
     )
